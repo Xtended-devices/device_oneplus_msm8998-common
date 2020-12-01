de9cc46e44fda698d50f83e259ae6ce9956b48da
mukesh22584 <mks22584@gmail.com>
2020-12-01 11:25:56 +0530

    mm8998-common: inherit Pixel 5 apn
    Ref:https://github.com/Project-Xtended/vendor_xtended/commit/45db9848f7356089798703f9fc5857e9723ce8ed

--- common.mk
+++ common.mk
@@ -24,6 +24,7 @@
 
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+$(call inherit-product, vendor/xtended/telephony/telephony.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
